b425ff78ee5c375b0ef70a4ec4f910e5289725fc
phbai <48466846@qq.com>
2017-08-18 18:25:33 +0800

    提交修改

--- web/src/components/node/node-list/index.js
+++ web/src/components/node/node-list/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import NodeListItem from '../node-list-item/index'
 import { Row, Col, message } from 'antd';
+import './index.css'
 
 class NodeList extends React.Component {
 
@@ -10,9 +11,9 @@ class NodeList extends React.Component {
       <div className="node-list">
         <div className="node-list-header">
             <Row type="flex" justify="space-between">
-                <Col span={11}>文件名</Col>
-                <Col span={2}>文件大小</Col>
-                <Col span={11} className="text-align-right">上传时间</Col>
+                <Col span={11} className="node-list-header-title">文件名</Col>
+                <Col span={2} className="node-list-header-title">文件大小</Col>
+                <Col span={11} className="node-list-header-title text-align-right">上传时间</Col>
             </Row>
         </div>
         {this.props.list && this.props.list.map((item) => {
